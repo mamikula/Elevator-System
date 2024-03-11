1e414359a1cdba6b6ebc0300a0e80c5114a15fa8
malym <mmikula@student.agh.edu.pl>
2024-03-11 19:27:37 +0100

    package refactor

--- backend/src/main/java/avs/recruitment/elevators/config/CORSConfig.java
+++ backend/src/main/java/avs/recruitment/elevators/config/CORSConfig.java
@@ -1,4 +1,4 @@
-package avs.recruitment.elevators;
+package avs.recruitment.elevators.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
